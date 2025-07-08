f31f6ed80a9ed6f7bf1f999fc353cc21c7273ec5
Fahmy Paiziah <officialfahmypaiziah@gmail.com>
2025-07-07 22:08:35 -0400

    error dummie
    ...

--- dummy.py
+++ dummy.py
@@ -1,3 +1,3 @@
 from BRScraper import nba
 # All players stats per game regular season
-print(nba.get_stats(2024, info='per_game', playoffs=False, rename=False))
+print(nba.get_stats(2024, info='per_game', playoffs=False, rename=False)
