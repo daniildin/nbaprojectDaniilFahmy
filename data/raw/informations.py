262a2def209ee9087950c7ac689f4ba52c156297
Fahmy Paiziah <officialfahmypaiziah@gmail.com>
2025-07-15 21:33:53 -0400

    superteam

---
+++ data/raw/informations.py
@@ -0,0 +1,37 @@
+'''
+Datas that we have so far:
+- Team stats information year 2000-2025
+- Team Salary Data year 2000-2021
+
+Source: Wikipedia
+
+A Superteam in the National Basketball Association (NBA) is an 
+arbitrary term to describe a team that uses player acquisitions 
+(via trades or free agency) in a short period of time to create a 
+higher than average concentration of top-level players. While there 
+is no official definition, it is generally viewed as a team with three 
+or more Hall of Fame, All-NBA, or perennial All-Star 
+players that join together to pursue an NBA championship.
+
+NBA Superteams(Notable Superteam): 
+
+Won Championships:
+    - 2007-2012: Boston Celtics
+    - 2010-2014: Miami Heat
+    - 2014-2017: Cleveland Cavaliers
+    - 2016-2019: Golden State Warriors
+
+No Championships(Notable Failed Superteam):
+    - 2003-2004: Los Angeles Lakers
+    - 2012-2013: Los Angeles Lakers
+    - 2013-2014: Brooklyn Nets
+    - 2017-2018: Oklahoma City Thunder
+    - 2021-2022: Brooklyn Nets
+    - 2021-2023: Los Angeles Lakers
+    - 2023-2024: Los Angeles Clippers
+    - 2023-2025: Phoenix Suns
+
+Criticism:
+
+
+'''
