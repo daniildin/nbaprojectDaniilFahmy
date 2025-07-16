1a5fd45c64cb2d09b109ec3a6206d4de7c43581d
Fahmy Paiziah <officialfahmypaiziah@gmail.com>
2025-07-15 21:49:36 -0400

    informations py

---
+++ data/raw/informations.py
@@ -0,0 +1,52 @@
+'''
+Datas that we have so far:
+- Team stats information year 2000-2025
+- Team Salary Data year 2000-2021
+
+Source: Wikipedia
+
+A Superteam in the National Basketball Association (NBA) is an 
+arbitrary term to describe a team that uses player acquisitions 
+(via trades or free agency) in a short period of time to create a 
+higher than average concentration of top-level players. While there 
+is no official definition, it is generally viewed as a team with three 
+or more Hall of Fame, All-NBA, or perennial All-Star 
+players that join together to pursue an NBA championship.
+
+NBA Superteams(Notable Superteam): 
+
+Won Championships:
+    - 2007-2012: Boston Celtics
+    - 2010-2014: Miami Heat
+    - 2014-2017: Cleveland Cavaliers
+    - 2016-2019: Golden State Warriors
+
+No Championships(Notable Failed Superteam):
+    - 2003-2004: Los Angeles Lakers
+    - 2012-2013: Los Angeles Lakers
+    - 2013-2014: Brooklyn Nets
+    - 2017-2018: Oklahoma City Thunder
+    - 2021-2022: Brooklyn Nets
+    - 2021-2023: Los Angeles Lakers
+    - 2023-2024: Los Angeles Clippers
+    - 2023-2025: Phoenix Suns
+
+Criticism:
+
+There has been some controversy about superteams in the NBA. 
+Claims persist that superstar players are no longer looking to 
+make the game competitive, but rather finding an easier way to win 
+championships and nullifying smaller contenders by joining other elites.
+From James joining the Heat to Durant joining the Warriors, there has been 
+derision from the media and fans in their efforts to unbalance the NBA. 
+However, there have been cases for both sides as others argue that the NBA 
+has achieved its highest grossing TV revenue, that it was all done under 
+the rules of the salary cap, and that it is enjoyable sports entertainment. 
+Recent efforts made to punish teams that look to assemble long-term superteams, 
+starting in the 2023-24 season, have been implemented under newly established 
+salary cap rules and limitations at hand. One such effort/method made was the 
+creation of the second tax apron, which looks to significantly hamper teams that 
+would look to overspend in order to create their superteams in question.
+
+
+'''
