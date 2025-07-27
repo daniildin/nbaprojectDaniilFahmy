665bfc06abdb0ccdaa4515b146afc9133d7747e3
Fahmy Paiziah <officialfahmypaiziah@gmail.com>
2025-07-27 18:36:15 -0400

    new update

---
+++ data/KeyQuestions.py
@@ -0,0 +1,14 @@
+"""
+🧠 Key Questions:
+Do superteams win more games than balanced teams?
+- Are superteams more expensive per win (Cost per Win)?
+- Do they generate more revenue per win (Revenue Efficiency)?
+- Are superteams “boom or bust”?
+(i.e., do they have higher variance in results?)
+
+💡 Why It Matters:
+This helps:
+- Fans understand if superteams are actually dominant or just hyped.
+- Owners/front offices evaluate ROI from assembling superstar rosters.
+- Analysts discuss team-building strategies in terms of performance vs cost.
+"""
